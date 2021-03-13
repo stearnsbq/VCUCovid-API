33a612038da3f7db110604db2433ec0f5738cb32
Quinn <stearnsbq@vcu.edu>
2021-03-12 21:35:25 -0500

    fix

--- scraper/scrape_sympomatic_tests.js
+++ scraper/scrape_sympomatic_tests.js
@@ -2,9 +2,11 @@ const util = require('../util');
 module.exports = function (model, ul, date) {
 	const contents = ul.contents;
 
+	
+
 	for (const li of contents.slice(0, 2)) {
         const strong = li.find('strong');
-		const type = li.getText().replace(/:/g, '').split(' ')[0];
+		const type = li.getText().replace(/:/g, '').replace(/&nbsp;/g, ' ').split(' ')[0];
         const count = parseInt(strong.text.replace(',', ''));
 		util.create(model[`symptomatic${type}Model`], date, count);
 	}
